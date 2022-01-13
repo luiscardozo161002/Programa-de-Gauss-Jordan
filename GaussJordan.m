a34db75f97de53789811a3507c80db2e899f661c
@Rardo <78442724+luiscardozo161002@users.noreply.github.com>
2022-01-13 11:19:26 -0600

    Update GaussJordan.m

--- GaussJordan.m
+++ GaussJordan.m
@@ -16,24 +16,24 @@ disp('::Metodo de Eliminacion de Gauss Jordan::');
 %Imprimimos el mensaje de instrucciones
 disp('::Ingrese la Matriz Aumentada::');
 %La funcion input nos permite ingresa la matriz aumentada A∗=(A|b) (valore de los coeficientes|valores de los terminos indipendiente)
-luisA=input ('A x=(A|b): ');
+luis_A=input ('A x=(A|b): ');
 %Desarrollo de un ciclo anidado for para trabajar con filas y columnas de la matriz-[nxm]
 %Hacemos un for que va desde la casilla 1,1 hasta n,1 
-for i=1:length(luisA(:,1)) 
+for i=1:length(luis_A(:,1)) 
     %Condicionamos sí el pivote es distinto a 1
-     if luisA(i,i)~=1 
+     if luis_A(i,i)~=1 
     %Lo dividimos entre el mismo para obtener el pivote en 1 
-        luisA(i,:)= luisA(i,:)./luisA(i,i);
+        luis_A(i,:)= luis_A(i,:)./luis_A(i,i);
     %Fin del condicional  
      end 
 %Hacemos un for anidado desde el (1,1) 
-for j=1:length(luisA(:,1)) 
+for j=1:length(luis_A(:,1)) 
     %Sí j es diferente a i significa que no esta en el pivote
     if j~=i  
     %Hacemos 0 las fila 
-      luisA(j,:)=-luisA(j,i).*luisA(i,:)+luisA(j,:);
+      luis_A(j,:)=-luis_A(j,i).*luis_A(i,:)+luis_A(j,:);
     %Imprimimos el resultado de la matriz en el espacio de trabajo 
-     disp(luisA)
+     disp(luis_A)
     %Espacio entre resultados de matriz 
      disp('') 
     end %if
